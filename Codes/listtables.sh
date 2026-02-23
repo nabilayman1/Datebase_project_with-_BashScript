d394137fd13d932f56c6074c7df6277324487cf4
Nabil Ayman <132666445+nabilayman1@users.noreply.github.com>
2026-02-23 23:19:50 +0200

    Delete Codes directory
    i changed in repo

--- Codes/listtables.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-mypath="$HOME/proj/DateBase/"$1""
-if [[ -n "$(find "$mypath" -mindepth 1 -maxdepth 1 -type f)" ]]; then
-    echo "Table you have :"
-    ls -l $HOME/proj/DateBase/$1 | tail -n +2 | awk '{print "-Table name:",$9,"-created by:",$3,"-creation date:",$8,$7,$6}' 
-else
-    echo "You don't have any Tables"  
-fi
